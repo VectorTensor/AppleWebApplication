38722275205ede1ae3ae0c79d2297f4cdec63592
VectorTensor <prayashthapa15@gmail.com>
2024-04-06 11:21:21 +0545

    feat(rabbit): rabbit mq communication sucess

---
+++ Apple/JsonDto/AppleQuality.cs
@@ -0,0 +1,16 @@
+﻿namespace Apple.JsonDto;
+
+public class AppleQuality
+{
+
+    public string Message { get; set; }
+    public string Quality { get; set; }
+
+    public AppleQuality(string m, string q)
+    {
+        Message = m;
+        Quality = q;
+
+    }
+    
+}
---
+++ Apple/Services/StringExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace Apple.Services;
+
+public static class AMa
+{
+
+    public static string UpperCase(this string s, int x)
+    {
+
+        
+        return char.ToUpper(s[0]) + s.Substring(1) + x.ToString();
+
+
+    }
+    
+}
